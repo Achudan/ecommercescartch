1f185d8ad71797cb510d1eba2269fc2693a00955
Achudan <achuts4@gmail.com>
2020-09-19 12:30:35 +0530

    achu comment

--- DB/Connection.js
+++ DB/Connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 //mongo URI
 const URI = "mongodb+srv://dbHulk:dbHulk@cluster0.zpedc.mongodb.net/<dbname>?retryWrites=true&w=majority";
-
+//connection
 const connectDB = async()=>{
     await mongoose.connect(URI,{ useNewUrlParser: true , useUnifiedTopology: true})
     console.log("Atlas db is successfullyconnected...")
