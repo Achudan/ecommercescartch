47eda7d4091aa51f44cf937d4fbb84af0e110580
Achudan <achuts4@gmail.com>
2020-09-28 15:07:41 +0530

    filterproducts Achu

--- Controller/HomeController.js
+++ Controller/HomeController.js
@@ -1,55 +1,28 @@
-const ecomm= require('../Model/schema');
+const ecomm = require('../Model/schema');
 const productValidator = require('../Services/ProductValidators');
 
-exports.getCategories = async(req,res) => {
+exports.getCategories = async (req, res) => {
     console.log("home")
-    ecomm.categoryModel.find((err,obj)=>{
-        if(obj==null)
+    ecomm.categoryModel.find((err, obj) => {
+        if (obj == null)
             res.status(204).send("No Categories Found")
-        else{
+        else {
             res.status(200).send(obj)
         }
     })
 }
 
-exports.filterProducts = async(req,res)=>{
-    //console.log(req.body.filterData)
-    let filteredData=[]
-    filteredData.push({"fullName":"hi","presentIn":"Categories"})
-    ecomm.categoryModel.find({category:{$regex:".*"+req.body.filterData+".*"}},(err,obj)=>{
-        //console.log(obj)
-        filteredData.push({"fullName":"hi","presentIn":"Categories"})
-        obj.forEach(data=>{
-            console.log(data.category)
-            filteredData.push({
-                "fullName":data.category,
-                "presentIn":"Categories"
-            })
-        })
+exports.filterProducts = async (req, res) => {
+    let filteredData = []
+    let value = await ecomm.categoryModel.find({ category: { $regex: ".*" + req.body.filterData + ".*" } })
+    value.forEach((data)=>{
+        filteredData.push({
+                    "fullName": data.category,
+                    "presentIn": "Categories"
+                })
     })
-    console.log(filteredData)
+    res.send(filteredData)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
